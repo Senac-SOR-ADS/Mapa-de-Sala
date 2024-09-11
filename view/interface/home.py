e570ac1d3e58e407e9bb0eb5f8a8e2d43de3028d
Bruno Rodrigues <alastroboy2000@gmail.com>
2024-09-11 11:19:45 -0300

    Alterações na Main e login.py
    alterei o login.py importando os dados do Vitor para o meu para assim eu fazer os testes de conexão com a home, quando o login ser feito redirecionar para a home

---
+++ view/interface/home.py
@@ -0,0 +1,21 @@
+# from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget)
+# from view.interface.login import Ui_Login
+# import sys
+
+# class Login(QWidget, Ui_Login):
+#     def __init__(self) -> None:
+#         super(Login, self).__init__()
+#         self.setupUi(self)
+#         self.setWindowTitle("Login")
+
+# class MainWindow(QMainWindow, Ui_Login):
+#     def __init__(self):
+#         super(MainWindow, self).__init__()
+#         self.setupUi(self)
+#         self.setWindowTitle("Login")
+
+# if __name__=="__main__":
+#     app = QApplication(sys.argv)
+#     window = Login()
+#     window.show()
+#     app.exec__()
