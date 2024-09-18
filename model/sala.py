89d8a005a7da4dde5eb5642aebe8801920dbe1dc
NickolasXL <nickolasalvesdequeiroz2007@gmail.com>
2024-09-18 11:53:22 -0300

    Update sala.py
    Foi feito a funcionalidade das funções (buscar_sala, buscar_sala_predio1 e buscar_sala_predio2) falta corrigir a funcionalidade da função cadastrar_sala

--- model/sala.py
+++ model/sala.py
@@ -2,6 +2,8 @@ from conexao import ConexaoBD
 
 
 class Sala:
+    __banco = ConexaoBD()
+
     def __init__(self, nome_da_sala, tipo, predio, equipamentos, capacidade, observacao) -> None:
         self.nome_da_sala = nome_da_sala
         self.tipo = tipo
@@ -9,10 +11,63 @@ class Sala:
         self.__equipamentos = equipamentos
         self.capacidade = capacidade
         self.observacao = observacao
-        self.__banco = ConexaoBD()
+        
  
     def get_equipamentos(self):
         return self.__equipamentos
    
     def  set_equipamentos(self, equipamentos):
         self.__equipamentos = equipamentos
+
+    @classmethod
+    def buscar_sala(cls):
+        cls.__banco.conectar()
+        query = '''SELECT * FROM sala;'''
+        resultado = cls.__banco.buscarTodos(query)
+        cls.__banco.desconectar()
+        return resultado
+    
+    @classmethod
+    def buscar_sala_predio1(cls):
+        cls.__banco.conectar()  
+        query = ''' SELECT * FROM `sala` WHERE predio = 1; '''
+        resultado = cls.__banco.buscarTodos(query)
+        cls.__banco.desconectar()
+        return resultado
+    
+    @classmethod
+    def buscar_sala_predio2(cls):
+        cls.__banco.conectar()
+        query = '''SELECT * FROM `sala` WHERE predio = 2;'''
+        resultado = cls.__banco.buscarTodos(query)
+        cls.__banco.desconectar
+        return resultado
+    
+    @classmethod
+    def cadastrar_sala(cls):
+        cls.__banco.conectar()
+        query = '''INSERT INTO `sala`(`nome_da_sala`, `tipo`, `predio`, `equipamentos`, `capacidade`, `observacao`) VALUES (%s, %s, %s, %s, %s, %s); '''
+        parametros = (cls.nome_da_sala, cls.tipo, cls.predio, cls.__equipamentos, cls.capacidade, cls.observacao)
+        resultado = cls.__banco.commit(query, parametros)
+        cls.__banco.desconectar()
+        return resultado
+    
+if __name__ == "__main__":
+    sala = Sala.cadastrar_sala()
+    print(sala)
+
+
+if __name__ == "__main__":
+    teste_buscar_sala = Sala.buscar_sala()
+    print(teste_buscar_sala)
+
+
+if __name__ == "__main__":
+    buscar_sala_predio1 = Sala.buscar_sala_predio1()
+    print(buscar_sala_predio1)
+
+
+if __name__ == "__main__":
+    buscar_sala_predio2 = Sala.buscar_sala_predio2()
+    print(buscar_sala_predio2)
+    
