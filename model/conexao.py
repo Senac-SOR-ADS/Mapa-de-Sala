5fd8542aba5b5b2f93f33fc5b414752cc68dd3ed
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2024-09-18 09:53:47 -0300

    Removido os prints de debug

--- model/conexao.py
+++ model/conexao.py
@@ -28,17 +28,14 @@ class ConexaoBD:
             if not self.__conexao().is_connected():
                 raise Exception('Não foi conectado')
 
-            print("Conectado")  # Debug
             return self.__conexao().is_connected()
 
         except connector.Error as err:
             self.__conn = None
-            print(f"Erro: {err}")
             return False
 
     def desconectar(self) -> bool:
         if self.__conexao().is_connected():
-            # print("Desconectado") # Debug
             self.__conexao().close()
 
     def buscar(self, query, param=None) -> list:
@@ -47,9 +44,7 @@ class ConexaoBD:
             cur.execute(query, param)
             resultado = cur.fetchone()
 
-            print(f"Busca única: {resultado}")
         except Exception as e:
-            print(f"Erro ao buscar um: {e}")
             resultado = list()
         finally:
             if cur:
@@ -62,9 +57,7 @@ class ConexaoBD:
             cur.execute(query, param)
             resultado = cur.fetchall()
 
-            print(f"Busca completa: {resultado}")
         except Exception as e:
-            print(f"Erro ao buscar todos: {e}")
             resultado = list()
 
         finally:
@@ -77,11 +70,10 @@ class ConexaoBD:
             cur = self.__conexao().cursor()
             cur.execute(query, param)
             self.commit()
-            print("Dados alterados com sucesso")
             return cur
 
         except Exception as e:
-            print(f"Erro ao alterar dados: {e}")
+            cur = None
 
     def commit(self):
         self.__conexao().commit()
