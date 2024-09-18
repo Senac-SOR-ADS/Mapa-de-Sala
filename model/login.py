98bf987c41d62f0b4ae8f449aee50ecc37866778
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2024-09-18 09:16:12 -0300

    validar login funcionando
    Realizei alterações necessárias para validar um login do banco e retornar o id do próprio login, e o id de pessoa.

--- model/login.py
+++ model/login.py
@@ -2,61 +2,55 @@ from conexao import ConexaoBD
 
 
 class Login:
-    def __init__(self, pessoa, email, senha) -> None:
-        self.__pessoa = pessoa
-        self.email = email
-        self.senha = senha
+    def __init__(self, email, senha) -> None:
+        self.__email = email
+        self.__senha = senha
+        self.__idLogin = None
+        self.__idPessoa = None
         self.__banco = ConexaoBD()
- 
-<<<<<<< Updated upstream
-    def get_pessoa(self):
-        return self.__pessoa
-   
-    def set_pessoa(self, pessoa):
-        self.__pessoa = pessoa
-=======
+
+    def get_idLogin(self):
+        return self.__idLogin
+
+    def set_idLogin(self, id):
+        self.__idLogin = id
+    
+    def get_idPessoa(self):
+        return self.__idPessoa
+
+    def set_idPessoa(self):
+        self.__idPessoa
+
+    def getEmail(self):
+        return self.__email
+
+    def getSenha(self):
+        return self.__senha
+
     def validarLogin(self):
         self.__banco.conectar()
 
-        busca = self.__banco.buscar("SELECT * FROM login WHERE email = %s AND senha = %s;", (self.getEmail(), self.getSenha()) )
+        query = "SELECT * FROM login WHERE email = %s AND senha = %s;"
+        parametros = (self.getEmail(), self.getSenha())
+        resultado = self.__banco.buscar(query,  parametros)
 
         self.__banco.desconectar()
 
         try:
-            if self.getEmail() == busca[2] and self.getSenha() ==  busca[3]:
-                self.__idLogin = busca[0]
-                self.__idPessoa = busca[1]
+            if self.getEmail() == resultado[2] and self.getSenha() ==  resultado[3]:
+                self.set_idLogin(resultado[0])
+                print(f"id Login: {resultado[0]}")
+                self.__idPessoa = resultado[1]
+                print(f"id Pessoa: {resultado[1]}")
                 return True
         except:
             return False
-    
-    def get_idLogin(self):
-        return self.__idLogin
-    
-    def get_idPessoa(self):
-        return Pessoa.getIdPessoa()
-    
-    def getEmail(self):
-        return self.__email
-    
-    def getSenha(self):
-        return self.__senha
-    
-    def compararLogin(self):
-        self.__banco.conectar()
-
-        query = 'SELECT * FROM pessoas WHERE idPessoa = %s'
-
-        self.__banco.buscar(query, (self.get_idPessoa(), ))
-
-
 
 if __name__ == "__main__":
     login = Login(email='emailgenerico@gmail.com', senha='senha456')
-    login.validarLogin()
-        # print(login.get_idPessoa())
-        # print('login com sucesso')
-        # print(login.compararLogin())
+    if login.validarLogin():
+        print("validado!")
+    else:
+        print("erro")
 
     # print(type(login.validarLogin()))
->>>>>>> Stashed changes
