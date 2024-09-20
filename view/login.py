f9dc701f623345d3f5664c0e1d37470ae3c233c9
VitorSilva06 <164203021+VitorSilva06@users.noreply.github.com>
2024-09-20 09:08:38 -0300

    Organização, adicionando função pyqtSlot
    Mudando o arquivo Login.py para a pasta view, e adicionando o pyqStlod() para conectar o botão btnEtrar com a função getEmailSenha. Aerada tambem a resposta do login caso contem alhum informação nos campos de inputs (inputEmail, inputSenha) .

--- view/login.py
+++ view/login.py
@@ -1,36 +1,35 @@
 
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import Qt
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import Qt, QTimer, pyqtSlot
 
 
 class LoginInterface(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi('interfaceLogin.ui',self)
+        loadUi('view/ui/interfaceLogin.ui',self)
         
         # Remove a barra de título e as bordas da janela
         self.setWindowFlags(Qt.FramelessWindowHint)
 
         # Define a janela como transparente
         self.setAttribute(Qt.WA_TranslucentBackground)
+    
 
-        #Conectando os botões:
-        self.btnEntrar.clicked.connect(self.getEmailSenha)
-
+    @pyqtSlot()
+    def on_btnEntrar_clicked(self):
+        self.getEmailSenha()
 
     def getEmailSenha(self):
         email = self.inputEmail.text()
         senha = self.inputSenha.text()
 
         if (email != '' and senha !=''):
-            print(email, senha)
+            self.respostaLogin.setText('LOGANDO...')
         else:
             texto = 'Dados incompletos'
             self.respostaLogin.setText(texto)
             QTimer.singleShot(2000, lambda: self.limparCampos(self.respostaLogin))
-            
 
 
         self.limparCampos(self.inputEmail)
