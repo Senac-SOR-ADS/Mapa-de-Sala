9d3d83fe4a02bca8d55170ce9cbcab709c1995fd
GABRIEL ALCANTARA DIAS PRESTES <gabriel.adprestes@senacsp.edu.br>
2024-09-20 11:42:47 -0300

    Aplicando funções
    Fiz um arquivo para a criptografia de senhas, junto com a implementação de queries para o arquivo de reservas do banco de dados

--- model/reserva.py
+++ model/reserva.py
@@ -56,51 +56,63 @@ class Reserva:
         self.__horaFim = horaFim
 
     def fazer_reserva(self):
+        """Uma função para você tentar fazer uma reserva, caso já exista uma reserva no mesmo horário, dia e sala, ele alerta você. Caso contrário ele faz a reserva"""
         self.__banco.conectar()
-        query_verifica = "SELECT * FROM reserva WHERE hrInicio BETWEEN %s AND %s AND dia = %s AND idSala = %s OR hrFim BETWEEN %s AND %s"
-        parametros_verifica = (self.__horaInicio, self.__horaFim, self.__dia, self.__idSala, self.__horaInicio, self.__horaFim)
+        query_verifica = "SELECT * FROM reserva WHERE idSala = %s AND dia = %s AND ((hrInicio < %s AND hrFim > %s) OR (hrInicio >= %s AND hrFim <= %s))"
+        parametros_verifica = (self.__idSala, self.__dia, self.__horaFim, self.__horaInicio, self.__horaInicio, self.__horaFim)
         resultado = self.__banco.buscarTodos(query_verifica, parametros_verifica)
         if resultado != []:
             print("Dentro desse intervalo já existe uma reserva")
+            return False
         else:
             query = "INSERT INTO reserva (idLogin, idPessoa, idCurso, idSala, dia, hrInicio, hrFim, observacao) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
             parametros = (self.__idLogin, self.__idPessoa, self.__idCurso, self.__idSala, self.__dia, self.__horaInicio, self.__horaFim, self.__observacao)
             self.__banco.alterarDados(query, parametros)
             self.__banco.desconectar()
+            return True
 
     def retornar_reserva(self):
+        """Uma função para devolver os dados da tabela de reserva do banco"""
         self.__banco.conectar()
         query = "SELECT * FROM reserva"
-        self.__banco.buscarTodos(query)
+        resultado = self.__banco.buscarTodos(query)
         self.__banco.desconectar()
+        return resultado
     
     def retornar_reserva_login(self):
+        """Retorna as reservas de um dia, junto com o horário da reserva, o nome de quem fez a reserva e a observação da reserva"""
         self.__banco.conectar()
-        query = "SELECT p.nome, r.hrInicio, r.hrFim, r.dia FROM reserva r JOIN login l ON r.idLogin = l.idLogin JOIN pessoa p ON l.idPessoa = p.idPessoa WHERE r.dia = %s; "
+        query = "SELECT p.nome, r.hrInicio, r.hrFim, r.dia, r.observacao FROM reserva r JOIN login l ON r.idLogin = l.idLogin JOIN pessoa p ON l.idPessoa = p.idPessoa WHERE r.dia = %s; "
         parametro = ([self.__dia])
-        self.__banco.buscarTodos(query, parametro)
+        resultado = self.__banco.buscarTodos(query, parametro)
         self.__banco.desconectar()
+        return resultado
 
     def retorna_reserva_pessoa(self):
+        """Retorna as reservas de um dia, junto do horário da reserva, nome da pessoa que vai usar a reserva e a observação da reserva"""
         self.__banco.conectar()
-        query = "SELECT p.nome, r.hrInicio, r.hrFim, r.dia FROM reserva r JOIN pessoa p ON p.idPessoa = r.idPessoa WHERE r.dia = %s; "
+        query = "SELECT p.nome, r.hrInicio, r.hrFim, r.dia, r.observacao FROM reserva r JOIN pessoa p ON p.idPessoa = r.idPessoa WHERE r.dia = %s; "
         parametro = ([self.__dia])
-        self.__banco.buscarTodos(query, parametro)
+        resultado = self.__banco.buscarTodos(query, parametro)
         self.__banco.desconectar()
+        return resultado
     
     def retorna_reserva_sala(self):
+        """Retorna o dia da reserva, junto do nome da sala, nome da pessoa que vai usar, horário da reserva, nome do curso e observação"""
         self.__banco.conectar()
-        query = "SELECT r.dia, s.nome AS nomeSala, p.nome AS nomeDocente, r.hrInicio, r.hrFim, c.oferta AS curso FROM reserva r JOIN sala s ON r.idSala = s.idSala JOIN pessoa p ON r.idPessoa = p.idPessoa JOIN curso c ON r.idCurso = c.idCurso; "
-        self.__banco.buscarTodos(query)
+        query = "SELECT r.dia, s.nome AS nomeSala, p.nome AS nomeDocente, r.hrInicio, r.hrFim, c.oferta AS curso, r.observacao FROM reserva r JOIN sala s ON r.idSala = s.idSala JOIN pessoa p ON r.idPessoa = p.idPessoa JOIN curso c ON r.idCurso = c.idCurso; "
+        resultado = self.__banco.buscarTodos(query)
         self.__banco.desconectar()
+        return resultado
     
     def retorna_reserva_dia(self):
+        """Retorna as reservas de um dia, com o nome da pessoa que vai usar, horário da reserva, nome do curso e observação"""
         self.__banco.conectar()
-        query = "SELECT p.nome, r.hrInicio, r.hrFim, r.dia, c.nome FROM reserva r JOIN pessoa p ON p.idPessoa = r.idPessoa JOIN curso c ON r.idCurso = c.idCurso WHERE r.dia = %s"
+        query = "SELECT p.nome, r.hrInicio, r.hrFim, r.dia, c.nome, r.observacao FROM reserva r JOIN pessoa p ON p.idPessoa = r.idPessoa JOIN curso c ON r.idCurso = c.idCurso WHERE r.dia = %s"
         parametro = ([self.__dia])
-        self.__banco.buscarTodos(query, parametro)
+        resultado = self.__banco.buscarTodos(query, parametro)
         self.__banco.desconectar()
+        return resultado
 
 if __name__ == "__main__":
-    teste = Reserva(1, 3, 5, 15, '2024/09/20', '09:00:00', '10:00:00')
-    teste.fazer_reserva()
+    pass
