73db50cab218fd7dfc134b6c5b2431da9d11594c
NickolasXL <nickolasalvesdequeiroz2007@gmail.com>
2024-09-20 11:46:38 -0300

    Update sala.py
    pequenas alterações.

--- model/sala.py
+++ model/sala.py
@@ -4,8 +4,8 @@ from conexao import ConexaoBD
 class Sala:
     __banco = ConexaoBD()
 
-    def __init__(self, nome_da_sala, tipo, predio, equipamentos, capacidade, observacao) -> None:
-        self.nome_da_sala = nome_da_sala
+    def __init__(self, nome, tipo, predio, equipamentos, capacidade, observacao) -> None:
+        self.nome = nome
         self.tipo = tipo
         self.predio = predio
         self.__equipamentos = equipamentos
@@ -46,28 +46,29 @@ class Sala:
     @classmethod
     def cadastrar_sala(cls):
         cls.__banco.conectar()
-        query = '''INSERT INTO `sala`(`nome_da_sala`, `tipo`, `predio`, `equipamentos`, `capacidade`, `observacao`) VALUES (%s, %s, %s, %s, %s, %s); '''
-        parametros = (cls.nome_da_sala, cls.tipo, cls.predio, cls.__equipamentos, cls.capacidade, cls.observacao)
-        resultado = cls.__banco.commit(query, parametros)
+        query = '''INSERT INTO `sala`(`nome`, `tipo`, `predio`, `equipamentos`, `capacidade`, `observacao`) VALUES (%s, %s, %s, %s, %s, %s); '''
+        parametros = (cls.nome, cls.tipo, cls.predio, cls.__equipamentos, cls.capacidade, cls.observacao)
+        resultado = cls.__banco.execute(query, parametros)
+        cls.__banco.commit()
         cls.__banco.desconectar()
         return resultado
-    
-if __name__ == "__main__":
-    sala = Sala.cadastrar_sala()
-    print(sala)
+   
+# if __name__ == "__main__":
+#     sala = Sala.cadastrar_sala()
+#     print(sala)
 
 
-if __name__ == "__main__":
-    teste_buscar_sala = Sala.buscar_sala()
-    print(teste_buscar_sala)
+# if __name__ == "__main__":
+#     teste_buscar_sala = Sala.buscar_sala()
+#     print(teste_buscar_sala)
 
 
-if __name__ == "__main__":
-    buscar_sala_predio1 = Sala.buscar_sala_predio1()
-    print(buscar_sala_predio1)
+# if __name__ == "__main__":
+#     buscar_sala_predio1 = Sala.buscar_sala_predio1()
+#     print(buscar_sala_predio1)
 
 
-if __name__ == "__main__":
-    buscar_sala_predio2 = Sala.buscar_sala_predio2()
-    print(buscar_sala_predio2)
+# if __name__ == "__main__":
+#     buscar_sala_predio2 = Sala.buscar_sala_predio2()
+#     print(buscar_sala_predio2)
     
