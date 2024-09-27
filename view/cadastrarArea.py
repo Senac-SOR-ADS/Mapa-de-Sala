323f214107f7049fb895c072d869bc9e9f7d2d31
Bruno Rodrigues <alastroboy2000@gmail.com>
2024-09-27 03:34:22 -0300

    Cadastro da área
    Tela de cadastro de área adicionado (ainda em desenvolvimento)

---
+++ view/cadastrarArea.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt, QTimer, pyqtSlot
+
+class CadastrarArea(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi('view/ui/cadastroArea.ui',self)
+
+    @pyqtSlot()
+    def on_btnCadastrarArea_clicked(self):
+        return self.getCadastroArea()
+    
+    def getCadastroArea(self):
+        area = self.cadastrarArea.text().strip()
+
+        return(area)
+
+
+    def validandoDados(self):
+        self.respostaCadastrando.setText('CADASTRANDO...')
+        QTimer.singleShot(2000, lambda: self.limparCampos(self.respostaCadastrando))
+
+    def dadosInvalidos(self):
+        texto = 'DADOS INCOMPLETOS.'
+        self.respostaNaoCadastrado.setText(texto)
+        QTimer.singleShot(2000, lambda: self.limparCampos(self.respostaNaoCadastrado))
+
+    def limparCampos(self, campo):
+        campo.clear()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = CadastrarArea()
+    widget.show()
+    app.exec_()
