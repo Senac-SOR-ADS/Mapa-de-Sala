fcf12c4841121d91325445da5accb313a23fe2b5
Bruno Rodrigues <alastroboy2000@gmail.com>
2024-09-27 10:43:30 -0300

    Código Atualizado
    Código Atualizado da Home e cadastros

--- view/home.py
+++ view/home.py
@@ -1,13 +1,13 @@
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 
-class CadastrarSalas(QMainWindow):
+class HomePrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi('view/ui/cadastroSalas.ui',self)
+        loadUi('view/ui/interfaceHomeV1.ui',self)
 
 if __name__ == "__main__":
     app = QApplication([])
-    widget = CadastrarSalas()
+    widget = HomePrincipal()
     widget.show()
     app.exec_()
