f13ee5a6bfdf378d0e400f8186becc2895432998
VitorSilva06 <164203021+VitorSilva06@users.noreply.github.com>
2024-09-27 11:53:50 -0300

    correção do codigo

--- view/home.py
+++ view/home.py
@@ -1,14 +1,32 @@
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
+from cadastrandoPessoas import cadastrandoPessoas
+from reserva import ReservaInterface
+
+
 class HomePrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi('view/ui/interfaceHomeV1.ui',self)
 
+   # Criando instancias das interfaces
+        self.interfCasPessoa = cadastrandoPessoas()
+        self.interfReserva = ReservaInterface()
+        self.inserirTelas( [self.interfCasPessoa, self.interfReserva] )
+
+        self.btnCadastrarPessoa.clicked.connect(lambda: self.trocarTela(self.interfCasPessoa))
+        self.btnReserva.clicked.connect(lambda: self.trocarTela(self.interfReserva))
+        self.btnIncio.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.inicio))
+
+    def inserirTelas(self, telas):
+        for interface in telas:
+            self.stackedWidget.addWidget(interface)
+
     def trocarTela(self, tela):
         """Função para trocar as tela. Necessario
         passar a classe da tela"""
         self.stackedWidget.setCurrentWidget(tela)
+        
         
 if __name__ == "__main__":
     app = QApplication([])
