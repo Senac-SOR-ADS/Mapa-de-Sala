d3fdeaaa8cecb045dd1068e8f90c59801e78f392
VitorSilva06 <164203021+VitorSilva06@users.noreply.github.com>
2024-10-02 07:55:51 -0300

    Criação de tela (Reserva, Cadastro de pessoas), Implementando troca de telas na home e mudança de tipo de interface ui   (#40)
    * Criação interface login
    
    Interface login criada, adicionado campos de senha e email juntamente com a imagem do logo senac.
    
    * Fundos transparente, verificação de dados
    
    Adicionado fundo transparente deixando somente o layout do login sem o do QT. Adicionado uma mensagem onde que se o usuario tentar fazer um login vazio, aparece a mensagem "dados invalidos" e tambem a função limparCampos que ela limpa os campos da interface após os dados serem enviados e após a mensagem aparecer por 2 dados invalidos aparecer por 2 segundos
    
    * Organização, adicionando função pyqtSlot
    
    Mudando o arquivo Login.py para a pasta view, e adicionando o pyqStlod() para conectar o botão btnEtrar com a função getEmailSenha. Aerada tambem a resposta do login caso contem alhum informação nos campos de inputs (inputEmail, inputSenha) .
    
    * alterada a respota e as estilização
    
    Alterada a resposta da função getEmailSenha quando os campos inputs (inputEmail, inputSenha) e as estilização, adicionando uma label para cada respostas.
    
    * alteração nas funções de mostrar na interface e estilização do botão entrar
    
    -alteração nas funções de mostrar na interface:
    Adicionando uma função para cada label.
    
    -estilização do botão entrar:
    Corregido o conflito onde não aparecia a estilização o botão entrar
    
    * conexao com a interface reserva
    
    criando metodos para fazer manipular as a interface e pegar dados.
    
    * Interface Cadastro de pessoa e conexão
    
    Criando a interface do cadastro pessoas juntamente com a conexão com os campos
    
    * trocando o tipo do arquivo para dialog
    
    trocando o tipo do arquivo para dialog
    
    * Redirecionamento de tela
    
    criando um conexão com o botão cadastrar Pessoas e reserva onde que redireciona para as respectivas telas
    
    * correção do codigo
    
    * Alterção dos arquivo Qmainwindow
    
    Trocando os arquivo cadastrodepessoas e reserva para QWidget
    
    ---------
    
    Co-authored-by: vitor2003back <161161431+vitor2003back@users.noreply.github.com>

---
+++ view/reserva.py
@@ -0,0 +1,47 @@
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt, QTimer, pyqtSlot
+
+
+
+class ReservaInterface(QWidget):
+    def __init__(self):
+        super().__init__()
+        loadUi('view/ui/reserva.ui',self)
+
+    def getDados(self)->dict:
+        """Pegando o dados na interface e retornando os valores"""
+        nomeDocenteResponsavel = self.nomeDocente.text().strip() 
+        nomeSala = self.salaReserva.text().strip() 
+        nomeCurso = self.cursoReserva.text().strip() 
+        equipamentos = self.equipamentosReserva.text().strip() 
+        inicio = self.inicioReserva.text().strip() 
+        fim = self.fimReserva.text().strip() 
+        observacao = self.observacaoReserva.text().strip() 
+        
+        dados = {"nomeDocente":nomeDocenteResponsavel, 
+                 "nomeSala":nomeSala, 
+                 "nomeCurso":nomeCurso,
+                 "equipamentos":equipamentos,
+                 "inicio":inicio,
+                 "fim":fim,
+                 "observações":observacao}
+        return dados
+    
+    @pyqtSlot()
+    def on_btnFazerReserva_clicked(self):
+        """Função para realizar os tratamentos dos dados após o click 
+        no botão btnFazerReserva(Reserva)"""
+        return self.getDados()
+        
+    def validandoDados(self):
+        self.feedbackReserva.setText('Reserva realizada.')
+        QTimer.singleShot(2000, lambda: self.limparCampos(self.feedbackReserva))
+
+    def dadosInvalidos(self):
+        self.feedbackReserva.setText('Dados incomoletos.')
+        QTimer.singleShot(2000, lambda: self.limparCampos(self.feedbackReserva))
+
+    def limparCampos(self, campo):
+        campo.clear()
+
