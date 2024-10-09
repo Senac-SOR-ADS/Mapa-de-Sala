965456bf7ef38e5a4da9820af38bfaf893311a89
GABRIEL ALCANTARA DIAS PRESTES <gabriel.adprestes@senacsp.edu.br>
2024-10-09 11:18:52 -0300

    Novo controller sala
    Adaptei, e corrigi o controller antigo de sala

--- App/view/cadastrarSalas.py
+++ App/view/cadastrarSalas.py
@@ -1,11 +1,14 @@
 from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
+from App.controller.sala import cadastrarSala
 
 
 class CadastrarSalas(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('App/view/ui/cadastroSalas.ui',self)
+
+        self.cadastrarSala.clicked.connect(self.iniciandoCadastro)
 
     def getCadastroSalas(self):
         nome = self.nomeSala.text().strip()
@@ -15,8 +18,16 @@ class CadastrarSalas(QWidget):
         capacidade = self.mediaCapacidade.text().strip()
         feedback = self.feedbackText.text().strip()
 
-        return(nome, sala, predio, equipamento, capacidade, feedback)
-
-
-        
+        return (nome, sala, predio, equipamento, capacidade, feedback)
     
+    def validarInputs(self):
+        campos = self.getCadastroSalas()
+        for i in campos:
+            if not i:
+                return False
+        return True
+    
+    def iniciandoCadastro(self):
+        if self.validarInputs():
+            nome, sala, predio, equipamento, capacidade, feedback = self.getCadastroSalas()
+            cadastrarSala(nome, sala, predio, equipamento, capacidade, feedback)
