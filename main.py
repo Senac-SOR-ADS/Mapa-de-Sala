e774fcbb89371deb087502ebce314743f92d251c
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2024-10-09 08:59:07 -0300

    Modificando a estrutura de import main
    Eliminando as dependências diretas da view na main

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-from PyQt5.QtWidgets import QApplication
-from controller.login import LoginController
-from controller.home import HomeController
-
 if __name__ == "__main__":
-    app = QApplication([])
-    login = LoginController()
-    
-    if login.exec_():
-        main = HomeController()
-    app.exec_()
-      
+    from App import desktop
+    # from App import web
