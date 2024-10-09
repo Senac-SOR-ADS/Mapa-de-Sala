890614cb038c6b21e6764f8772a94309f0044718
Bruno Mendes <bfmendesadm@gmail.com>
2024-10-09 11:35:37 -0300

    Login
    login usando conexão com o banco

--- App/routes/routes.py
+++ App/routes/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
+from App.controller.login import validarLogin
 
 # Definindo o blueprint
 routes = Blueprint('routes', __name__)
@@ -11,3 +12,15 @@ def main():
 @routes.errorhandler(404)
 def page_not_found(error):
     return render_template('error404.html'), 404
+
+@routes.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form["email"]
+        senha = request.form["senha"]
+        if validarLogin(email, senha):
+            print('redirecionar rota para home')
+            return render_template('home.html')
+        else:
+            print('nao deu..')
+    return render_template('login.html')
