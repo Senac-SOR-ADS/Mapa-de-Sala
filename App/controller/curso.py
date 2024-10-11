763f318b6e08bfc429c1adb5af6e4acb4574068d
otavio <164201968+taviohenrique@users.noreply.github.com>
2024-10-11 08:37:46 -0300

    controller curso

---
+++ App/controller/curso.py
@@ -0,0 +1,7 @@
+from App.model.curso import Curso
+
+def cadastrarCurso(area, nome, oferta, periodo, carga, horas, alunos):
+    cursoModel = Curso(nome, oferta, periodo, carga, horas, alunos)
+    if cursoModel.cadastrar_curso(area):
+        return True
+    return False
