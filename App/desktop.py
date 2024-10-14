7d8bf7fcb47ecf8d23b0ea5b237fb1533c46ef19
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2024-10-14 10:08:45 -0300

    voltando as mudanças desktop

--- App/desktop.py
+++ App/desktop.py
@@ -1,17 +1,13 @@
 from PyQt5.QtWidgets import QApplication
 from App.view.login import LoginInterface
 from App.view.home import HomePrincipal
-from App.view.cadastroPessoas import cadastroPessoas
 
 
 app = QApplication([])
-# login = LoginInterface()
+login = LoginInterface()
 
-# if login.exec_():
-#     main = HomePrincipal()
-#     main.show()
-
-cadaPessoa = cadastroPessoas()
-cadaPessoa.show()
-app.exec_()
+if login.exec_():
+    main = HomePrincipal()
+    main.show()
+    app.exec_()
     
