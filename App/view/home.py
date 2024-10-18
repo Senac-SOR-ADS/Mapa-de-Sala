ba6ee2f754e0d16af3344b5aed2efd26a43d90ab
Bruno Rodrigues <alastroboy2000@gmail.com>
2024-10-18 08:18:08 -0300

    alteração import
    alteração de import na Home

--- App/view/home.py
+++ App/view/home.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import Qt
 from .cadastroPessoas import cadastroPessoas
@@ -62,6 +62,7 @@ class HomePrincipal(QMainWindow):
 
         
 if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication
     app = QApplication([])
     widget = HomePrincipal()
     widget.show()
