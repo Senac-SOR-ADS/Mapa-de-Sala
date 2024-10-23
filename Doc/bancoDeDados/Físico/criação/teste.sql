6d8302f553a934a5f6f2d3a380f6a806dd464019
GABRIEL ALCANTARA DIAS PRESTES <gabriel.adprestes@senacsp.edu.br>
2024-10-23 08:25:53 -0300

    Arrrumando o sql do banco
    Só coloquei a propriedade unique no campo email da tabela de pessoa

--- Doc/bancoDeDados/Físico/criação/teste.sql
+++ Doc/bancoDeDados/Físico/criação/teste.sql
@@ -99,7 +99,7 @@ CREATE TABLE `pessoa` (
   `CPF_CNPJ` varchar(18) NOT NULL COMMENT 'CPF ou CNPJ',
   `nascimento` date NOT NULL COMMENT 'Data de nascimento',
   `telefone` varchar(15) NOT NULL COMMENT 'Telefone de contato',
-  `email` varchar(100) NOT NULL COMMENT 'Email da pessoa',
+  `email` varchar(100) NOT NULL UNIQUE COMMENT 'Email da pessoa',
   `cargo` varchar(50) NOT NULL COMMENT 'Cargo ou função da pessoa',
   PRIMARY KEY (`idPessoa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
