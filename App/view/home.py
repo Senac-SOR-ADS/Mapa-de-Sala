938251201973775511b07e96c37f55f4eb48effe
Surik4to06 <richardasso56@gmail.com>
2024-10-23 11:04:50 -0300

    colocando menu mais detalhado na home

--- App/view/home.py
+++ App/view/home.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QStackedWidget
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import Qt, pyqtSlot
 from .cadastroPessoas import cadastroPessoas
@@ -6,7 +6,7 @@ from .reserva import ReservaInterface
 from .cadastrarArea import CadastrarArea
 from .cadastrarCurso import CadastrarCurso
 from .cadastrarLogin import CadastroLogin
-
+from .cadastrarSalas import CadastrarSalas
 
 class HomePrincipal(QMainWindow):
     def __init__(self):
@@ -14,11 +14,17 @@ class HomePrincipal(QMainWindow):
         loadUi('App/view/ui/home.ui',self)
         self.moving = False
         self.subMenuLateral.hide()
+        self.subMenuQuebrado.hide()
         
    # Criando parte interativa do menu
    
         self.btnMenu: QPushButton
         self.subMenuLateral: QWidget
+        self.subMenuQuebrado: QWidget
+        self.menuQuebrado: QStackedWidget
+        self.cadastros: QWidget
+        self.busca: QWidget
+        self.editar: QWidget
 
    # Criando instancias das interfaces
         self.setWindowFlags(Qt.FramelessWindowHint)
@@ -27,9 +33,17 @@ class HomePrincipal(QMainWindow):
         self.interfCasArea = CadastrarArea()
         self.interfCasCurso = CadastrarCurso()
         self.interfCasLogin = CadastroLogin()
-        self.inserirTelas( [self.interfCasPessoa, self.interfReserva, self.interfCasArea, self.interfCasCurso, self.interfCasLogin] )
-
-        self.btnCadastrarPessoa.clicked.connect(lambda: self.trocarTela(self.interfCasPessoa))
+        self.interfcasSala = CadastrarSalas()
+        self.inserirTelas( [self.interfcasSala, self.interfCasPessoa, self.interfReserva, self.interfCasArea, self.interfCasCurso, self.interfCasLogin] )
+        
+    #Telas dentro do menu para alterar as janelas pelo sub menu
+        self.btnPessoa.clicked.connect(lambda: self.trocarTelaMenu(self.cadastros))
+        self.btnPesquisa.clicked.connect(lambda: self.trocarTelaMenu(self.busca))
+        self.btnEditar.clicked.connect(lambda: self.trocarTelaMenu(self.editar))
+        
+    #btns da propria interface   
+        self.btnCadastarSala.clicked.connect(lambda: self.trocarTela(self.interfcasSala))
+        self.btnCadastroPessoa.clicked.connect(lambda: self.trocarTela(self.interfCasPessoa))
         self.btnReserva.clicked.connect(lambda: self.trocarTela(self.interfReserva))
         self.btnIncio.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.inicio))
         self.btnArea.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.interfCasArea))
@@ -49,11 +63,24 @@ class HomePrincipal(QMainWindow):
     def inserirTelas(self, telas):
         for interface in telas:
             self.stackedWidget.addWidget(interface)
+    
+    def inserirTelasMenu(self, menu):
+        for i in menu:
+            self.menuQuebrado.addWidget(i)
 
     def trocarTela(self, tela):
         """Função para trocar as tela. Necessario
         passar a classe da tela"""
         self.stackedWidget.setCurrentWidget(tela)
+    
+    def trocarTelaMenu(self, menu):
+        if self.subMenuQuebrado.isVisible():
+            self.menuQuebrado.setCurrentWidget(menu)
+        else:
+            self.subMenuQuebrado.show()
+            self.menuQuebrado.setCurrentWidget(menu)
+            
+        
         
     def mousePressEvent(self, event):
         if event.button() == Qt.RightButton:
@@ -75,7 +102,10 @@ class HomePrincipal(QMainWindow):
             self.subMenuLateral.hide()
         else:
             self.subMenuLateral.show()
-
+    
+    @pyqtSlot()
+    def on_btnFecharMenuQuebrado_clicked(self):
+        self.subMenuQuebrado.hide()
         
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
