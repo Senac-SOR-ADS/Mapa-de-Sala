646340a873415075eec7247a996364783cbf8182
Bruno Rodrigues <alastroboy2000@gmail.com>
2024-10-23 15:45:12 -0300

    Funcionamento dos temas
    Com as alterações dos diretórios atuais agora é possível fazer a alteração relativa entre os temas

--- App/view/configurarTela.py
+++ App/view/configurarTela.py
@@ -10,24 +10,34 @@ class ConfigurarTela(QWidget):
         super().__init__()
         loadUi('App/view/ui/telaConfiguracoes.ui', self)
         
+        self.btnMudarTema.clicked.connect(self.mudarTema)
         # Instância do controlador dos temas
         self.temaController = TemaController()
-        self.temaController.temaMudado.connect(self.atualizarTema)
+        self.temaController.temaMudado.connect(self.funcaoTemaGlobal)
 
+
+
+        self.temas = [
+            'App/view/ui/css/temaClaro/temaPrincipal.css',
+            'App/view/ui/css/temaEscuro/temaPrincipal.css'
+        ]
         self.temaAtual = 0
+    
         
-        # Conecta à função de mudarTema
-        self.btnMudarTema.clicked.connect(self.atualizarTema)
+    def funcaoTemaGlobal(self, tema):
+        with open(tema, 'r') as f:
+            cssTema = f.read()
+        QApplication.instance().setStyleSheet(cssTema)
+
 
     def mudarTema(self):
         tema = self.temas[self.temaAtual]
-        self.temaAtual = (self.temaAtual + 1) % len(self.temas) # Atualiza o próximo tema
-        self.temaController.temaMudado.emit(tema) # Emite o sinal sobre as trocas de temas
+        self.temaAtual = (self.temaAtual + 1) % len(self.temas) 
+        self.temaController.temaMudado.emit(tema)
 
-    def atualizarTema(self):
-            self.setStyleSheet(open('App/view/ui/css/temaClaro/temaPrincipal.css').read()) 
-            self.setStyleSheet(open('App/view/ui/css/temaEscuro/temaPrincipal.css').read()) 
-            
+    def atualizarTema(self, tema):
+        with open(tema, 'r') as f:
+            self.setStyleSheet(f.read())
         
 if __name__ == "__main__":
     import sys
