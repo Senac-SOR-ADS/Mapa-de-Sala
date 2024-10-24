c41d7798ed81f44a50347686091a72b3eacd6c16
Bruno Rodrigues <alastroboy2000@gmail.com>
2024-10-24 01:21:45 -0300

    Alterações finais dessa branch
    Adicionado dois icones para serem exibido como "tema claro" e "tema escuro" e novas funções para serem exibidas o tema

--- App/view/configurarTela.py
+++ App/view/configurarTela.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5.QtCore import pyqtSignal, QObject, QTimer
 from PyQt5.uic import loadUi
 
 class TemaController(QObject):
@@ -10,50 +10,63 @@ class ConfigurarTela(QWidget):
         super().__init__()
         loadUi('App/view/ui/telaConfiguracoes.ui', self)
         
+        self.btnMudarTema.stateChanged.connect(self.mudarTema)
         # Instância do controlador dos temas
         self.temaController = TemaController()
         self.temaController.temaMudado.connect(self.funcaoTemaGlobal)
 
+        # Faz a exibição dos icones
+        self.btnMudarTema.setStyleSheet("""
+            QCheckBox::indicator {
+                width: 30px;
+                height: 30px;
+            }
+            QCheckBox::indicator:unchecked {
+                image: url("App/view/ui/icones/toggleOff.png");
+            }
+            QCheckBox::indicator:checked {
+                image: url("App/view/ui/icones/toggleOn.png");
+            }
+        """)
 
         self.temas = [
-            'App/view/ui/css/temaClaro/temaPrincipal.css',
-            'App/view/ui/css/temaEscuro/temaPrincipal.css'
+            'App/view/ui/css/temaEscuro/temaPrincipal.css',
+            'App/view/ui/css/temaClaro/temaPrincipal.css'
         ]
         self.temaAtual = 0
-
-        # Configura o estado inicial do checkbox
-        self.btnMudarTema.setChecked(self.temaAtual == 1)
-        self.atualizarTema()
-
-        # Conecta o estado do checkbox à função de mudar tema
-        self.btnMudarTema.stateChanged.connect(self.mudarTema)
     
-        
     def funcaoTemaGlobal(self, tema):
-        try:
-            with open(tema, 'r') as f:
-                cssTema = f.read()
-            # print(f'Conteúdo do CSS:\n{cssTema}')   
-            QApplication.instance().setStyleSheet(cssTema)
-            print(f'Tema aplicado')
-        except Exception as e:
-            print(f'Erro ao aplicar tema: {e}')
-
+        with open(tema, 'r') as f:
+            cssTema = f.read()
+        QApplication.instance().setStyleSheet(cssTema)
 
     def mudarTema(self):
-        #Atualiza o tema baseado no estado do checkbox
-        if self.btnMudarTema.isChecked():
-            self.temaAtual = 1  # Tema escuro
-        else:
-            self.temaAtual = 0
-        
         tema = self.temas[self.temaAtual]
+        self.temaAtual = (self.temaAtual + 1) % len(self.temas) 
         self.temaController.temaMudado.emit(tema)
 
-    def atualizarTema(self):
-        tema = self.temas[self.temaAtual]
-        self.temaController.temaMudado.emit(tema)
-        
+        self.atualizarMensagem()
+
+    def atualizarMensagem(self):
+        if self.temaAtual == 0: 
+            self.temaEscuro.setVisible(True)   
+            self.temaClaro.setVisible(False)  
+            self.mensagemTemaEscuro()
+        else:  
+            self.temaEscuro.setVisible(False)   
+            self.temaClaro.setVisible(True) 
+            self.mensagemTemaClaro()
+
+    def mensagemTemaClaro(self):
+        self.temaClaro.setText('Modo escuro')
+
+    def mensagemTemaEscuro(self):
+        self.temaEscuro.setText('Modo claro')
+
+    def atualizarTema(self, tema):
+        with open(tema, 'r') as f:
+            self.setStyleSheet(f.read())
+
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
