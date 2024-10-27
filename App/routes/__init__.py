4278e1b0d342dd78718457782cffb5bf53cf04b6
Bruno Mendes <bfmendesadm@gmail.com>
2024-10-26 22:50:23 -0300

    __init__.py
    Crie um __init__.py em routes/auth para definir os elementos acessíveis ao importar o pacote, incluindo funções e classes essenciais.
    Crie um __init__.py em routes para definir os elementos acessíveis ao importar o pacote.
    Crie register.py em routes para registrar todos os blueprints na aplicação Flask.
    Realize melhorias no CSS, especialmente para os formulários, tabelas e mensagens de flash.

--- App/routes/__init__.py
+++ App/routes/__init__.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, Flask
+# Importação do logger e rotas
 from .logger_setup import logger
-
 from .home import home_route
 from .pessoa import pessoa_route
 from .reserva import reserva_route
@@ -12,34 +11,19 @@ from .equipamento import equipamento_route
 from .auth.login import login_route
 from .auth.logout import logout_route
 
-# Registra todos os blueprints na aplicação Flask.
-def register_routes(app: Flask) -> None:
-    
-    # Lista de blueprints e seus respectivos prefixos de URL
-    blueprints = [
-        (home_route, '/'),
-        (login_route, '/login'),
-        (logout_route, '/logout'),
-        (pessoa_route, '/funcionario'),
-        (reserva_route, '/reserva'),
-        (relatorio_route, '/relatorio'),       
-        (sala_route, '/sala'),
-        (area_route, '/area'),
-        (curso_route, '/curso'),
-        (equipamento_route, '/equipamento')
-    ]
-
-    # Registra cada blueprint na aplicação
-    for blueprint, url_prefix in blueprints:
-        try:
-            app.register_blueprint(blueprint, url_prefix=url_prefix)
-            logger.info(f"Blueprint registrado: {blueprint} com o prefixo: {url_prefix}")
-        except Exception as e:
-            logger.error(f"Erro ao registrar blueprint: {blueprint} com o prefixo: {url_prefix}. Erro: {e}", exc_info=True)
-
-def check_template_access(app: Flask) -> None:
-    try:
-        app.jinja_env.get_template('/Home/home.html')
-        logger.info("Template 'home.html' encontrado com sucesso.")
-    except Exception as e:
-        logger.error(f"Erro ao acessar o template 'home.html': {e}", exc_info=True)
+# Definição dos elementos acessíveis ao importar o pacote
+__all__ = [
+    "logger",
+    "home_route",
+    "pessoa_route",
+    "reserva_route",
+    "relatorio_route",
+    "sala_route",
+    "area_route",
+    "curso_route",
+    "equipamento_route",
+    "login_route",
+    "logout_route",
+    "register_routes",
+    "check_template_access",
+]
