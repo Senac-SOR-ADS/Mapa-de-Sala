a1c51d31f1d673fb720fe1d67bcc63aa9c7b1b65
otavio <164201968+taviohenrique@users.noreply.github.com>
2024-11-06 11:45:12 -0300

    TROCA DE RESERVA QUASE PRONTA SE PA

--- App/controller/reserva.py
+++ App/controller/reserva.py
@@ -27,9 +27,13 @@ def validarCadastro(idLogin, dados, diasValidos):
     while diaAtual <= diaFim:
         diaSemana = diaAtual.weekday()
         if diasValidos[diaSemana]:
-            if not Reserva(idLogin, dados['idDocente'], dados['idCurso'], dados['idSala'], diaAtual, dados['inicioCurso'], dados['fimCurso'], dados['observações']).validar_dia_livre():
+            if not Reserva(idLogin, dados['idDocente'], dados['idCurso'], dados['idSala'], diaAtual, dados['inicioCurso'], dados['fimCurso'], dados['observações']).validar_periodo():
                 print(f'Na seguinte data já existe uma reserva: {diaAtual}')
                 return False
         diaAtual += timedelta(days=1)
     print('Todos os dias estão livres')
     return True
+
+def trocar_reserva(dados1, dados2):
+    if Reserva.atualizar(dados1['idLogin'], dados1['idPessoa'], dados1['idcurso'], dados1['idSala'], dados1['dia'], dados1['inicioCurso'], dados1['fimCurso'], dados1['observações'],  dados1['idReserva']):
+        Reserva.atualizar(dados2['idLogin'], dados2['idPessoa'], dados2['idcurso'], dados2['idSala'], dados2['dia'], dados2['inicioCurso'], dados2['fimCurso'], dados2['observações'],  dados2['idReserva'])
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
     from App import desktop
+    # from App.model.reserva import Reserva
+    # print(Reserva.validar_troca(11, '2024-11-07', '08:00:00', '10:00:00'))
     # from App import web
