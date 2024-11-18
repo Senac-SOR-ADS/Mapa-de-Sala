50929d2ed36203e009b9b8967a158bf334b6fff5
Bruno Mendes <bfmendesadm@gmail.com>
2024-11-18 09:25:59 -0300

    Ajuste Controller
    Fiz ajuste em controler Pessoa para Filtra os resultados com base no nome por em buscarPessoas.
    
    buscarPessoaId ajuste para retornar formatado CPF/CNPJ e Telefone. usa funções  CPF/CNPJ e Telefone criada em utils.py

--- App/controller/utils.py
+++ App/controller/utils.py
@@ -1,16 +1,32 @@
-# criada para controller Sala
+import re
+
+# =================== criada para controller Pessoa ===================
+
+# Função para modificar a data  
+def modificarData(dataNasc):
+    data = dataNasc.split('/')
+    return f'{data[2]}-{data[1]}-{data[0]}'
+
+# Função para formatar o CPF
+def formatarCpf(cpf):
+    return re.sub(r'(\d{3})(\d{3})(\d{3})(\d{2})', r'\1.\2.\3-\4', cpf)
+
+# Função para formatar o CNPJ
+def formatarCnpj(cnpj):
+    return re.sub(r'(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})', r'\1.\2.\3/\4-\5', cnpj)
+
+# Função para formatar o telefone
+def formatarTelefone(telefone):
+    return re.sub(r'(\d{2})(\d{4,5})(\d{4})', r'(\1) \2-\3', telefone)
+
+# =================== criada para controller Sala ===================
 def validarInputs(valores):
     for i in valores:
         if not i:
             return False
     return True
 
-# criada para controller Pessoa
-def modificarData(dataNasc):
-    data = dataNasc.split('/')
-    return f'{data[2]}-{data[1]}-{data[0]}'
-
-# criada para controller Pessoa para Reserva
+# =================== criada para controller Pessoa para Reserva ===================
 def modificarDataReserva(data):
     data = data.split('-')
     return f'{data[2]}/{data[1]}/{data[0]}'
