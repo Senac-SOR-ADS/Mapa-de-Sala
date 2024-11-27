a069ffd720c2bee381baacd116eac5fe65e1905f
otavio <164201968+taviohenrique@users.noreply.github.com>
2024-11-27 11:53:58 -0300

    começando a verificação da semana

--- App/controller/reserva.py
+++ App/controller/reserva.py
@@ -33,21 +33,24 @@ def validarCadastro(dados, diasValidos):
             if validar:
                 listaDias.append(validar[0])
         diaAtual += timedelta(days=1)
-    
     if listaDias != []:
         return listaDias
     return False
 
+# def validarSemana(segunda, terca, quarta, quinta, sexta, sabado):
+    
+
+
 def trocar_reserva(dados1, dados2):
     if Reserva.atualizar(dados1['idLogin'], dados1['idPessoa'], dados1['idcurso'], dados1['idSala'], dados1['dia'], dados1['inicioCurso'], dados1['fimCurso'], dados1['observações'],  dados1['idReserva']):
         Reserva.atualizar(dados2['idLogin'], dados2['idPessoa'], dados2['idcurso'], dados2['idSala'], dados2['dia'], dados2['inicioCurso'], dados2['fimCurso'], dados2['observações'],  dados2['idReserva'])
-        
-        
+    
     
 def deletarReserva(idReserva):
     if Reserva.deletar(idReserva):
         return True
     return False
+
 
 def atualizarReserva(idLogin, idPessoa, idCurso, idSala, dia, hrInicio, hrFim, observacao, idReserva):
     if Reserva.atualizar(idLogin, idPessoa, idCurso, idSala, dia, hrInicio, hrFim, observacao, idReserva):
