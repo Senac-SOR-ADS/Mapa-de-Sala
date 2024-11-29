47f0af60df828d9b0b1472f69cafbff94eb7db6d
NickolasXL <164202169+NickolasXL@users.noreply.github.com>
2024-11-29 08:39:26 -0300

    Revert "mover telas ainda a completar"
    This reverts commit 26b1921fff283de8b7a877e9e6e847a0f5e7650e.

--- App/view/home.py
+++ App/view/home.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QStackedWidget
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import Qt, pyqtSlot, QRect
+from PyQt5.QtCore import Qt, pyqtSlot
 
 # Interfaces
 from .cadastroPessoas import cadastroPessoas
@@ -23,11 +23,8 @@ class HomePrincipal(QMainWindow):
         super().__init__()
         loadUi('App/view/ui/home.ui',self)
         self.moving = False
-        self.resizing = False 
         self.subMenuLateral.hide()
         self.subMenuQuebrado.hide()
-        self.resize_margin = 5
-
         
    # Criando parte interativa do menu
    
@@ -145,57 +142,19 @@ class HomePrincipal(QMainWindow):
             
 
     def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            return
         if event.button() == Qt.LeftButton:
+            self.moving = True
             self.offset = event.pos()
-        widget_atual = self.childAt(event.pos())
-        
-            # Detectar clique no cabeçalho
-        if widget_atual and widget_atual.objectName() == "cabeçalho":
-                    self.moving = True
-        else:
-            # Detectar clique em bordas para redimensionar
-            if self.is_on_edge(event.pos()):
-                self.resizing = True
-                self.start_geometry = self.geometry()
-                self.start_pos = event.globalPos()
 
     def mouseMoveEvent(self, event):
         if self.moving and not self.isMaximized():
-            self.move(event.globalPos() - self.offset)
-        elif self.resizing:
-            delta = event.globalPos() - self.start_pos
-            new_width = max(self.start_geometry.width() + delta.x(), self.minimumWidth())
-            new_height = max(self.start_geometry.height() + delta.y(), self.minimumHeight())
-            self.resize(new_width, new_height)
-        else:
-            self.update_cursor(event.pos())  # Atualizar cursor dependendo da posição
+            self.move(self.pos() + event.pos() - self.offset)
 
     def mouseReleaseEvent(self, event):
         self.moving = False
-        self.resizing = False
-        self.unsetCursor()
-
-    def is_on_edge(self, pos):
-        """Detecta se o mouse está em uma borda para redimensionamento."""
-        rect = self.rect()
-        if QRect(rect.right() - self.resize_margin, rect.top(), self.resize_margin, rect.height()).contains(pos):
-            self.setCursor(Qt.SizeHorCursor)
-            return True
-        elif QRect(rect.left(), rect.bottom() - self.resize_margin, rect.width(), self.resize_margin).contains(pos):
-            self.setCursor(Qt.SizeVerCursor)
-            return True
-        elif QRect(rect.right() - self.resize_margin, rect.bottom() - self.resize_margin, self.resize_margin, self.resize_margin).contains(pos):
-            self.setCursor(Qt.SizeFDiagCursor)
-            return True
-        return False
-
-    def update_cursor(self, pos):
-        """Atualiza o tipo do cursor baseado na posição do mouse."""
-        if self.is_on_edge(pos):
-            return
-        self.unsetCursor()
-
-
+    
     @pyqtSlot()
     def on_btnFecharMenuQuebrado_clicked(self):
         self.subMenuQuebrado.hide()
