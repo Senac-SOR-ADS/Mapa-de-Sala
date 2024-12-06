6f20c65ffcb96d77ce233cbf038237473b4a4797
Prof-Rayner <jeff.rfsimoes@senacsp.edu.br>
2024-12-06 11:48:43 -0300

    Squashed commit of the following:
    commit 3646e3a2abb5eac5b28f6d9be22d11a135962ebf
    Author: Prof-Rayner <jeff.rfsimoes@senacsp.edu.br>
    Date:   Fri Dec 6 11:12:32 2024 -0300
    
        Inserção do modo de autenticação
    
        Inserido a funcionalidade para registrar o login feito pelo usuário, sendo necessário chamar a função pegarUsuarioLogado para obter as informações

--- App/desktop.py
+++ App/desktop.py
@@ -1,12 +1,16 @@
 from PyQt5.QtWidgets import QApplication
 from App.view.login import LoginInterface
 from App.view.home import HomePrincipal
+from App.controller.login import pegarUsuarioLogado
 
 
 app = QApplication([])
 login = LoginInterface()
 
-if login.exec_():
-    main = HomePrincipal()
-    main.show()
-    app.exec_()
+while pegarUsuarioLogado()['id_login'] == None:
+    if login.exec_():
+        main = HomePrincipal()
+        main.show()
+        app.exec_()
+    else:
+        break
