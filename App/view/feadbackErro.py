89f55ea95857d7b1f3ead7fe8d4d0d08c5e349a0
Surik4to06 <145988210+Surik4to06@users.noreply.github.com>
2024-12-09 09:45:09 -0300

    telas de confirmações (#181)
    * colocando menu mais detalhado na home
    
    * correção do sub menu alinhado pra cima agr
    
    * mudando icones
    
    mudando icones conforme as atividades na aplicação, tipo mecher no menu ou minimizar e maximizar, etc
    
    * arrumando algumas coisas so e os icones
    
    * colocando menu simplificado e arrumando tabs
    
    fiz um menu novo só com icones agr e arrumei o problema dos tabs tmb, alem de agr os botões ficarem clicados quando escolhem um deles
    
    * mudando nome dos icones e atualizando informações da home e agr puxando telas de edit
    
    * mudando os icones de erro e concluido
    
    * colocando caminho certo no icone de tela cheia
    
    * caminho do icone da tela maximizada agr
    
    * adicionando tela de edit sala
    
    * arrumando tabulação
    
    * mudança na ortografia da tela de cadastro de login
    
    * mudando o nome e zerando txt feadback
    
    * tirando algumas validações q nn funcionam
    
    * tela de concluido com 1 btn pra confirmar
    
    * .
    
    * tela de login funcionando sem erro agr
    
    * comentando erros na tela de login, para tratativa de erro
    
    * agr vai kkk
    
    * wqkjhfweijfgqwiuf
    
    * eygdafoawsd
    
    * colocando telas de validação e confirmação
    
    * passando as tratativas de sair da conta
    
    * mudando formas de chamar o icone  de erro

--- App/view/feadbackErro.py
+++ App/view/feadbackErro.py
@@ -1,9 +1,41 @@
 from PyQt5.QtWidgets import QDialog
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
 
 class FeadbackErro(QDialog):
-    def __init__(self):
+    def __init__(self, txt):
         super().__init__()
         loadUi('App/view/ui/feadbackErro.ui',self)
         
-    #label do aviso do erro = #texto
+        self.texto.setText(txt)
+        
+        # Remove a barra de título e as bordas da janela
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        
+        self.btnConfirmar.clicked.connect(self.reject)
+        self.btnFechar.clicked.connect(self.reject)
+        
+    def mudarFoto(self, img):
+        if img == 'Validado':
+            self.setStyleSheet("""
+                                #icon {
+                                    image: url(App/view/ui/icones/iconConcluido.png)
+                                }
+                                    """)
+        else:
+            self.setStyleSheet("""
+                                #icon {
+                                    image: url(App/view/ui/icones/iconErro.png)
+                                }
+                                    """)
+        
+        
+    
+if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication
+    resp = FeadbackErro('sla')
+    app = QApplication([])
+    if resp.exec_():
+        print('ok')
+    else:
+        print('negado')
