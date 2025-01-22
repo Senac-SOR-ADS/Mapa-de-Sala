3c06a1a278694e5e393655aa5f59d0accbc78a91
Surik4to06 <145988210+Surik4to06@users.noreply.github.com>
2025-01-22 09:18:42 -0300

    adicionando funcionalidade de ocultar e visualizar senhas  (#193)
    * adicionando botão para alterar visibilidade da senhas
    
    * arrumando um erro de inversão na senha e mudando a cor do titulo
    
    * colocando no if a comparação mais exata da senha com echoMode

--- App/view/cadastrarLogin.py
+++ App/view/cadastrarLogin.py
@@ -1,19 +1,33 @@
 from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
-
-
+ 
+ 
 class CadastroLogin(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('App/view/ui/cadastroLogin.ui',self)
-        
+       
+        self.btnMostrarSenha.clicked.connect(self.mostrarSenha)
+       
     #nome btn e inputs
-    
+   
     #input Email = email
     #input Senha = senha
     #btn cadastroLogin = btnCdastrarLogin
     #checkbox sla = nivelAcesso
     #msgs de erro ou sucesso = respostas
-    
-        
-        
+   
+    #Função para mudar visibilidade da senha
+    def mostrarSenha(self):
+        if self.senha.echoMode() == 2:
+           self.senha.setEchoMode(self.senha.EchoMode.Normal)
+           self.btnMostrarSenha.setStyleSheet('''
+                                              #btnMostrarSenha {
+                                                icon: url("App/view/ui/icones/iconOlhoFechado.png");    
+                                              }''')
+        else:
+            self.senha.setEchoMode(self.senha.EchoMode.Password)
+            self.btnMostrarSenha.setStyleSheet('''
+                                              #btnMostrarSenha {
+                                                icon: url("App/view/ui/icones/iconOlhoAberto.png");  
+                                              }''')
