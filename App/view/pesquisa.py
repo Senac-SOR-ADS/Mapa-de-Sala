ea12dbab90381ab756743ec86c389396ad76fede
Surik4to06 <richardasso56@gmail.com>
2025-02-07 11:48:54 -0300

    falta colocar o stack pra funcionar agr ,_,

--- App/view/pesquisa.py
+++ App/view/pesquisa.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QStackedWidget
 from PyQt5.uic import loadUi
 
 class TelaPesquisa(QWidget):
@@ -8,6 +8,12 @@ class TelaPesquisa(QWidget):
 
         self.dataInicio.setCalendarPopup(True)
         self.dataFim.setCalendarPopup(True)
+
+        self.stackReservas: QStackedWidget
+        self.reservaMultipla: QWidget
+
+        self.teste.clicked.connect(self.stackReservas.setCurrentWidget(self.reservaMultipla))
+        
 
 
 ###########NOME DOS BTNS E INPUTS ##############
