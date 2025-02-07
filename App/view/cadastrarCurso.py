c0892074cf818b136c3353f1b738480ce09aca11
Bruno Rodrigues <145821792+Tortugazil@users.noreply.github.com>
2025-02-07 10:10:52 -0300

    Arrumando Responsividade (#210)
    * mudando telas
    
    fiz alteração na tela de salas, cursos e editar cursos, porém a de cursos não farei commit agora pois ainda falta arrumar a questão do layout em um campo que está sobrepondo outro e na de sala só falta um campo para manter ele exato com os outros
    
    * alteração nos layouts
    
    novo padrão adicionado às telas para seguirem uma ordem ao serem estilizadas
    
    * Desenvolvimento das telas
    
    Arrumando layouts indefinidos aos quais antes não estavam sendo responsivos com base do tamanho da tela (em desenvolvimento!)
    
    * tela reescrita
    
    Refiz a tela de cadastroSalas para manter um layout mais consistente em relação à responsividade
    
    * revisões
    
    nomeando nomes dos campos para condizer com o Python e adicionado conexão do campo `nomeSala` no código Python
    
    * Ajustes de inconsistências
    
    Fiz alterações em algumas telas na questão de padronização de telas, comentei algumas linhas no código Python pois estavam sendo chamadas errado, estava causando inconsistência e estava causando conflitos com o arquivo .ui, função denominada de """combox""".  Por último a tela de `editarLogin` ficou com o campo em layout à parte por conta da imagem `pixMap` que desconfigura a questão de escala, não retirei pois está ligada a uma função em que não fui o responsável por implementar
    
    * Modificando tela de salas
    
    Ajuste feito com a ajuda da equipe de Banco de Dados para condizer corretamente com os campos

--- App/view/cadastrarCurso.py
+++ App/view/cadastrarCurso.py
@@ -4,31 +4,31 @@ from PyQt5.QtCore import QTimer, pyqtSlot
 from App.controller.curso import cadastrarCurso
 from App.controller.area import listarAreas
 from App.controller.utils import validarAcao
-
+ 
 class CadastrarCurso(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('App/view/ui/cadastroCurso.ui',self)
         self.dicionarioDeAreas = listarAreas()
         self.popularJanela()
-        
+       
     def popularJanela(self):
         self.comboxArea()
-        
+       
     @pyqtSlot()
     def on_btnCadastrarCurso_clicked(self):
         info = self.getCadastroCurso()
         idArea = self.dicionarioDeAreas[info[0]]
         if cadastrarCurso(idArea, info):
             validarAcao()
-            
-    
-
+           
+   
+ 
     def comboxArea(self):
         areas = self.dicionarioDeAreas.keys()
         self.campoArea.addItems(areas)
-            
-        
+           
+       
     def getCadastroCurso(self):
         area = self.campoArea.currentText().strip()
         nome = self.nomeCurso.text().strip()
@@ -37,21 +37,20 @@ class CadastrarCurso(QWidget):
         carga = self.cargaCurso.text().strip()
         horas = self.horasPorDia.text().strip()
         alunos = self.quantidadeAlunos.text().strip()
-        
+       
         return(area, nome, oferta, periodo, carga, horas, alunos)
-    
-    
-
+   
+   
+ 
     def validandoDados(self):
         self.respostas.setText('CADASTRANDO...')
         QTimer.singleShot(2000, lambda: self.limparCampos(self.respostas))
-
+ 
     def dadosInvalidos(self):
         texto = 'DADOS INCOMPLETOS.'
         self.respostas.setText(texto)
         QTimer.singleShot(2000, lambda: self.limparCampos(self.respostas))
-
+ 
     def limparCampos(self, campo):
         campo.clear()
-
-
+ 
