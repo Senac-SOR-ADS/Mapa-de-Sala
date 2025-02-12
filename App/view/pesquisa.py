57263461b81d3171f8c050ac555d423d0cb56fb5
Gabriel Alcantara Dias Prestes <gabriel.260207@gmail.com>
2025-02-12 11:51:21 -0300

    Populando Tela
    Adicionei funções e informações na tela de pesquisa

--- App/view/pesquisa.py
+++ App/view/pesquisa.py
@@ -1,13 +1,31 @@
-from PyQt5.QtWidgets import QWidget, QStackedWidget
+from PyQt5.QtWidgets import QWidget, QStackedWidget, QDateEdit
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import QDate
+
+from App.controller.curso import listarCurso
+from App.controller.sala import listarSala
 
 class TelaPesquisa(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('App/view/ui/pesquisa.ui',self)
 
+        self.dicionarioCursos = listarCurso()
+        self.dicionarioDeSala = listarSala()
+
+        self.dataInicio = self.findChild(QDateEdit, 'dataInicio') 
+        self.dataFim = self.findChild(QDateEdit, 'dataFim')
         self.dataInicio.setCalendarPopup(True)
         self.dataFim.setCalendarPopup(True)
+
+        self.dataInicio.setDisplayFormat('dd/MM/yyyy')
+        self.dataInicio.setDate(QDate.currentDate())
+        self.dataFim.setDisplayFormat('dd/MM/yyyy')
+        self.dataFim.setDate(QDate.currentDate())
+        self.setDataMinima()
+        self.popularTela()
+
+        self.dataInicio.dateChanged.connect(self.setDataMinima)
 
         self.stackReservas: QStackedWidget
         self.reservaMultipla: QWidget
@@ -17,6 +35,22 @@ class TelaPesquisa(QWidget):
 
     def trocarTela(self, tela:QWidget):
         self.stackReservas.setCurrentWidget(tela)
+
+    def setDataMinima(self):
+        data = self.dataInicio.date()
+        self.dataFim.setMinimumDate(data)
+
+    def comboboxOferta(self):
+        ofertas = self.dicionarioCursos.keys()
+        self.campoOferta.addItems(ofertas)
+
+    def comboboxSala(self):
+        salas = self.dicionarioDeSala.keys()
+        self.campoSala.addItems(salas)
+
+    def popularTela(self):
+        self.comboboxOferta()
+        self.comboboxSala()
 
         
 
