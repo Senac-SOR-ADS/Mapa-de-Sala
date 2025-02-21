c24b1156f8461a1045069a706b7d969f9f993398
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2025-02-21 10:39:00 -0300

    melhorando chamada do CardPesquisa
    adicionando uma variável. Puramente para legibilidade

--- App/view/pesquisa.py
+++ App/view/pesquisa.py
@@ -135,7 +135,8 @@ class TelaPesquisa(QWidget):
         coluna = 0
         linha = 0
         for dados in reservas:
-            grid.addWidget(CardPesquisa(dados, self.dicionarioDeSala, self.dicionarioCursos), linha, coluna)
+            card = CardPesquisa(dados, self.dicionarioDeSala, self.dicionarioCursos)
+            grid.addWidget(card, linha, coluna)
             coluna += 1
             if coluna == max_colunas:
                 coluna = 0
