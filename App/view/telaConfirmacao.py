b90b67791ab65ada1b2e1b4be8d1744437889e6e
NickolasXL <164202169+NickolasXL@users.noreply.github.com>
2025-02-26 09:47:20 -0300

    alteração

--- App/view/telaConfirmacao.py
+++ App/view/telaConfirmacao.py
@@ -1,12 +1,14 @@
 from PyQt5.QtWidgets import QDialog
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 
 
 class TelaConfirmacao(QDialog):
     def __init__(self, titulo, aviso, txtBtnOk, equipamentos=False):
         super().__init__()
         loadUi('App/view/ui/telaConfirmacao.ui', self)
+        self._is_dragging = False
+        self._start_pos = QPoint()
         
         self.checkBoxEquipamentos.setStyleSheet("""
             QCheckBox::indicator {
@@ -41,6 +43,22 @@ class TelaConfirmacao(QDialog):
             self.containerEquipamentos.show()
         else:
             self.containerEquipamentos.hide()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self._is_dragging = True
+            self._start_pos = event.globalPos() - self.frameGeometry().topLeft()
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        if self._is_dragging and event.buttons() == Qt.LeftButton:
+            self.move(event.globalPos() - self._start_pos)
+            event.accept()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self._is_dragging = False
+            event.accept()
         
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
