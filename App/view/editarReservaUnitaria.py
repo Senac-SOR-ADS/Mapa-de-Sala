4b0d82627229505b4e3389c6cf3fe3a47cbdbad4
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2025-03-07 09:54:28 -0300

    Populando campos editarReserva

--- App/view/editarReservaUnitaria.py
+++ App/view/editarReservaUnitaria.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog, QDateEdit, QApplication
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import Qt
+from App.controller.sala import listarSala
+from App.controller.pessoa import buscarPessoas
 
 from App.model.reserva import Reserva
  
@@ -8,7 +10,26 @@ class ReservaUnitaria(QDialog):
     def __init__(self, idReserva):
         super().__init__()
         loadUi('App/view/ui/editarReservaUnitaria.ui',self)
-        self.popularCampos(idReserva)
+        self.dadosConsultados = {
+            'pessoas': buscarPessoas(),
+            'salas': listarSala(),
+        }
 
-    def popularCampos(self, idReserva):
-        Reserva.retornar_uma_reserva(idReserva)
+        print("id da reserva", idReserva)
+        self.popularJanela()
+
+    def popularJanela(self):
+        self.comboBoxPessoa()
+        self.comboBoxSala()
+
+    def comboBoxPessoa(self):
+        """Busca as pessoas no banco e popula o comboBox."""
+        pessoas = self.dadosConsultados['pessoas'] #buscarPessoas()
+        self.nomeDocente.clear()
+        self.nomeDocente.addItems(pessoas.keys())
+
+    def comboBoxSala(self):
+        """Busca as salas no banco e popula o comboBox."""
+        salas = self.dadosConsultados['salas'] #listarSala()
+        self.salaReserva.clear()
+        self.salaReserva.addItems(salas.keys())
