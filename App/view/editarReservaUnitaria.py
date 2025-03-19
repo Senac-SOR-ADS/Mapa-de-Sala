8234300de3a2bed711b009a849a8a4a0b1198295
Samuel Henricky <86206263+S4mK206@users.noreply.github.com>
2025-03-19 15:27:29 -0300

    alterações pendentes
    eu Samuel, vacilei em subir os arquivos para a entrega do projeto integrador.
    gorotiei

--- App/view/editarReservaUnitaria.py
+++ App/view/editarReservaUnitaria.py
@@ -4,6 +4,10 @@ from PyQt5.QtCore import Qt, QDate, QTime, pyqtSlot
 from App.controller.pessoa import buscarPessoas
 from App.controller.sala import listarSala
 from App.controller.reserva import getReserva
+from App.model.reserva import Reserva
+from App.controller.curso import listar_id_e_nome_cursos
+from App.controller.utils import modificarData
+from App.view.feedback import Feedback
 
 class ReservaUnitaria(QDialog):
     def __init__(self, idReserva, dataReserva):
@@ -13,6 +17,7 @@ class ReservaUnitaria(QDialog):
         self.dadosConsultados = {
             'pessoas': buscarPessoas(),
             'salas': listarSala(),
+            'cursos': listar_id_e_nome_cursos(),
             'pessoaAtual': self.dadosReserva[2],
             'salaAtual': self.dadosReserva[4],
         }
@@ -23,27 +28,36 @@ class ReservaUnitaria(QDialog):
         self.fim = self.getHoraFormatada(self.dadosReserva[7])
         self.observacao = self.dadosReserva[9]
         self.curso = self.dadosReserva[3]
+        self.login = self.dadosReserva[1]
+        self.idReserva = idReserva
 
         self.diaInicio.setCalendarPopup(True)
         self.diaInicio.setDisplayFormat('dd/MM/yyyy')
         self.diaInicio.setMinimumDate(QDate.currentDate())
         self.inicioCurso.timeChanged.connect(self.setMinimoHoraFim)
 
-        self.popularJanela(self.inicio, self.fim, self.observacao)
+        # self.popularJanela(self.inicio, self.fim, self.observacao)
+        self.comboBoxPessoa()
+        self.setCurso()
+        self.comboBoxSala()
         self.setDataReserva()
+        self.setHoraInicio(self.inicio)
+        self.setHoraFim(self.fim)
+        self.setMinimoHoraFim()
+        self.setObservacao(self.observacao)
 
         # print("id da reserva", idReserva)
         # print('pessoaAtual', self.dadosReserva[2])
         # print(self.dadosConsultados['pessoas'])
 
-    def popularJanela(self, horaInicio, horaFim, observacao):
-        self.comboBoxPessoa()
-        self.setCurso()
-        self.comboBoxSala()
-        self.setHoraInicio(horaInicio)
-        self.setHoraFim(horaFim)
-        self.setMinimoHoraFim()
-        self.setObservacao(observacao)
+    # def popularJanela(self, horaInicio, horaFim, observacao):
+    #     self.comboBoxPessoa()
+    #     self.setCurso()
+    #     self.comboBoxSala()
+    #     self.setHoraInicio(horaInicio)
+    #     self.setHoraFim(horaFim)
+    #     self.setMinimoHoraFim()
+    #     self.setObservacao(observacao)
         
     def get_indice_pessoa_atual(self):
         pessoas = self.dadosConsultados['pessoas']
@@ -69,12 +83,38 @@ class ReservaUnitaria(QDialog):
         self.nomeDocente.setCurrentIndex(pessoa_atual)
 
         # print(f'indice pessoa atual: {pessoa_atual}')
+    
+    def pegar_docente_selecionado(self):
+        docentes = self.dadosConsultados['pessoas']
+        indice = self.nomeDocente.currentIndex()
+        id = list(docentes.keys())[indice] or 0
+        return id
+    
+    def pegar_sala_selecionada(self):
+        salas = self.dadosConsultados['salas']
+        indice = self.salaReserva.currentIndex()
+        id = list(salas.values())[indice] or 0
+        return id
+    
+    def pegar_hora(self, horaTimedelta):
+        stringHoras = str(horaTimedelta)
+        hora = int(stringHoras.split(':')[0])
+        minuto = int(stringHoras.split(':')[1])
+        return f"{hora:02d}:{minuto:02d}"
+    
+    def pegar_hora_inicio_selecionada(self):
+        inicio = self.dadosReserva[6]
+        indice = self.inicioCurso.currentIndex()
+        id = list(inicio.values())[indice] or 0
+
 
     def setCurso(self):
-        curso = self.curso
-        print(curso)
-        # self.nomeCurso.clear()
-        # self.nomeCurso.setText(curso)
+        id_curso = self.curso
+        cursos = self.dadosConsultados['cursos']
+        self.nomeCurso.clear()
+        nomeCurso = cursos[id_curso]
+        self.nomeCurso.setText(nomeCurso)
+        # print(curso)
 
     def comboBoxSala(self):
         """Busca as salas no banco e popula o comboBox."""
@@ -108,22 +148,26 @@ class ReservaUnitaria(QDialog):
    
     def setObservacao(self, txt):
         self.observacaoReserva.setText(txt)
+    def getObservacao(self):
+        return self.observacaoReserva.text()
 
     @pyqtSlot()
     def on_btnEditarReserva_clicked(self):
-        docente = self.dadosConsultados['pessoaAtual']
-        curso = self.curso
-        sala = self.dadosConsultados['salaAtual']
-        hrInicio = self.inicio
-        hrFim = self.fim
-        diaInicio = self.diaInicio.date().toString('dd/MM/yyyy')
-        obs = self.observacao
+        login = self.login
+        docente = self.pegar_docente_selecionado()
+        sala = self.pegar_sala_selecionada()
+        diaInicio = modificarData(self.diaInicio.text().strip())
+        id_curso = self.curso
+        hrInicio = self.inicioCurso.text()
+        hrFim = self.fimCurso.text()
+        obs = self.getObservacao()
 
+        resultado = Reserva.atualizar(login, docente, id_curso, sala, diaInicio, hrInicio, hrFim, True, obs, self.idReserva)
+        if resultado:
+            resposta = Feedback(True, 'Atualização efetuado', 'Reserva atualizada', 'atualizado')
+            resposta.exec_()
+            self.close()
+        else:
+            resposta = Feedback(False, 'Erro ao Atualizar', 'não atualizado', 'não atualizado')
+            resposta.exec_()
 
-        print(f"IDs do docente: {docente}, curso: {curso}, sala: {sala}, hrInicio: {hrInicio}, hrFim: {hrFim}, diaInicio: {diaInicio} obs: {obs}")
-        
-        # docente
-        # sala
-        # hr inicio
-        # hr fim
-        # data
